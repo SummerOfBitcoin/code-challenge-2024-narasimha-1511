73560295586d1c729b8549650c777cd56c4239c2
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-04-15 18:10:11 +0530

    again

--- solution/Helpers/Block/createBlock.js
+++ solution/Helpers/Block/createBlock.js
@@ -8,15 +8,13 @@ function createBlock(merkle_root, nonce) {
   serialize += "01000000"; // Version -> 4 bytes -> Little Endian
   serialize += (0).toString(16).padStart(64, "0"); // Previous Block Hash -> 32 bytes -> Natural byte order
   serialize += merkle_root.match(/../g).reverse().join(""); // Merkle Root -> 32 bytes -> Natural Byte Order
-  const Time = parseInt(
-    parseInt(Date.now() / 1000) + Math.random() * (3600 - 1) + 1
-  );
+  const Time = parseInt(Date.now() / 1000);
   serialize += Time.toString(16)
     .padStart(8, "0")
     .match(/../g)
     .reverse()
     .join(""); // Time -> 4 bytes -> Little Endian
-  serialize += "20176682"; // Bits -> 4 bytes -> Little Endian -> this is Current Target
+  serialize += "0000ffff"; // Bits -> 4 bytes -> Little Endian -> this is Current Target
   serialize += nonce.toString(16).padStart(8, "0"); // Nonce -> 4 bytes -> Little Endian
   // serialize += transactions.length.toString(16).padStart(2, "0"); // Number of Transactions -> 1 byte -> Little Endian
 
