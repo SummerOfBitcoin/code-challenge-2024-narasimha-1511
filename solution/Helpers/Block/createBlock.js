878baed6843a50d368ed7f5b3055b9056573cf8c
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-04-15 20:11:22 +0530

    a

--- solution/Helpers/Block/createBlock.js
+++ solution/Helpers/Block/createBlock.js
@@ -7,7 +7,7 @@ function createBlock(merkle_root, nonce) {
   let serialize = "";
   serialize += "11000000"; // Version -> 4 bytes -> Little Endian
   serialize += (0).toString(16).padStart(64, "0"); // Previous Block Hash -> 32 bytes -> Natural byte order
-  serialize += merkle_root.match(/../g).reverse().join(""); // Merkle Root -> 32 bytes -> Natural Byte Order
+  serialize += merkle_root; // Merkle Root -> 32 bytes -> Natural Byte Order
   const Time = Math.floor(Date.now() / 1000);
   serialize += Time.toString(16)
     .padStart(8, "0")
